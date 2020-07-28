ad3407eb4e96838c8432b4be47e4f834689c6837
Nick G <nick.g@implemica.com>
2020-07-29 01:30:04 +0300

    минус деприкейт на билд градле и добавил логгер в гл. классе

--- src/main/java/com/gk/logistic/GKLogistic.java
+++ src/main/java/com/gk/logistic/GKLogistic.java
@@ -2,11 +2,13 @@ package com.gk.logistic;
 
 import com.gk.logistic.proxy.ServerProxy;
 import com.gk.logistic.util.Reference;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
 
 /**
  * The entry class to the GKLogic mod.
@@ -14,8 +16,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
  * @author Gribiwe
  */
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
-public class GKLogistic
-{
+public class GKLogistic {
 
     @Mod.Instance
     public static GKLogistic instance;
@@ -23,15 +24,15 @@ public class GKLogistic
     @SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.SERVER, modId = Reference.MODID)
     public static ServerProxy proxy;
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    private static Logger logger;
 
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-
+    public void init(FMLInitializationEvent event) {
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
