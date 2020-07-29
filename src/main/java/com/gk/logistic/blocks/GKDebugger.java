6bff1aacf27f4ed48ad9f55c0c1a5e30279eb699
Martelly <kulbanikolai@yandex.ru>
2020-07-29 18:11:19 +0300

    Нормальное разделение прокси и доработка по блокам

---
+++ src/main/java/com/gk/logistic/blocks/GKDebugger.java
@@ -0,0 +1,12 @@
+package com.gk.logistic.blocks;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+
+public class GKDebugger extends BlockBase{
+
+    public GKDebugger() {
+        super("gkdebugger_block", Material.CAKE, CreativeTabs.BUILDING_BLOCKS);
+
+    }
+}
