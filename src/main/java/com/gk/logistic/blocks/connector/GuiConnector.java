e6a90ed1365c6bfd28a619618d2594b7e3172ca5
Martelly <kulbanikolai@yandex.ru>
2020-07-31 19:32:53 +0300

    Добавлена кнопка в гуи для коннектора

--- src/main/java/com/gk/logistic/blocks/connector/GuiConnector.java
+++ src/main/java/com/gk/logistic/blocks/connector/GuiConnector.java
@@ -1,7 +1,9 @@
 package com.gk.logistic.blocks.connector;
 
+import com.gk.logistic.GKLogistic;
 import com.gk.logistic.init.ModBlocks;
 import com.gk.logistic.util.Constants;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
@@ -9,6 +11,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextComponentString;
 
 public class GuiConnector extends GuiContainer {
 
@@ -42,6 +45,33 @@ public class GuiConnector extends GuiContainer {
         String name = I18n.format(ModBlocks.TUBE.getUnlocalizedName() + ".name");
         this.fontRenderer.drawString(name, 8, 6, 000000);
         this.fontRenderer.drawString(this.playerInv.getDisplayName().getFormattedText(), 8, this.ySize - 92, 000000);
+    }
+
+    //This method is called when the Gui is first called!
+    @Override
+    public void initGui()
+    {
+        //You have to add this line for the Gui to function properly!
+        super.initGui();
+
+        //The parameters of GuiButton are(id, x, y, width, height, text);
+        this.buttonList.add(new GuiButton( 1, this.guiLeft + 123, this.guiTop + 4, 48, 13, "Start"));
+        //NOTE: the id always has to be different or else it might get called twice or never!
+
+        //Add any other buttons here too!
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton b)
+    {
+        //If the button id is different, or you have mrs buttons, create another if block for that too!
+        if(b.id == 1)
+        {
+            te.getWorld()
+                    .getClosestPlayer(te.getPos().getX(), te.getPos().getY(), te.getPos().getZ(), 10, false)
+            .sendMessage(new TextComponentString("Hello world"));
+
+        }
     }
 
 
